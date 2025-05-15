47fe30f51da242f12d530727cc8e88fb3bf50228
Alijono24 <azamjonnajottalim@gmail.com>
2025-05-15 12:34:40 +0500

    Modellar ForeignKey bilan bog'landi

---
+++ pupils/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Pupil, Sinf
+# Register your models here.
+
+class AdminPupil(admin.ModelAdmin):
+    search_fields = ['name', 'fam']
+    list_filter = ['age']
+
+
+admin.site.register(Sinf)
+admin.site.register(Pupil, AdminPupil)
---
+++ pupils/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Sinf(models.Model):
+    name = models.CharField(max_length=20)
+    max_pupils = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Pupil(models.Model):
+    name = models.CharField(max_length=20)
+    fam = models.CharField(max_length=20)
+    age = models.IntegerField()
+    sinf = models.ForeignKey(Sinf, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} {self.fam}"
